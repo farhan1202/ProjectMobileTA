7a6f9bd10948fb01219109c52c7088822705fccd
farhan1202 <hafifi1202@gmail.com>
2020-07-17 16:58:31 +0700

    Add TabLayout

--- app/src/main/java/com/dev/projectta/intro/LoginActivity.java
+++ app/src/main/java/com/dev/projectta/intro/LoginActivity.java
@@ -1,12 +1,16 @@
 package com.dev.projectta.intro;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dev.projectta.R;
+import com.dev.projectta.home.MainActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,5 +29,22 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editNoBP.getText().toString().equals("admin") && editPassword.getText().toString().equals("admin")){
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void register(View view) {
+        Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+        startActivity(intent);
     }
 }
