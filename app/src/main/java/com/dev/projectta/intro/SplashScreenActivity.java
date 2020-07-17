e6a92d41f43cb34496fabec8efa63ac18391274f
farhan1202 <hafifi1202@gmail.com>
2020-07-17 15:12:28 +0700

    Add Layout For SplashScreen, LoginActivity, RegisterActivity

---
+++ app/src/main/java/com/dev/projectta/intro/SplashScreenActivity.java
@@ -0,0 +1,50 @@
+package com.dev.projectta.intro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.dev.projectta.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+
+        splash();
+    }
+
+    public void splash(){
+        Thread thread = new Thread(){
+            public void run(){
+                try {
+                    sleep(4000);
+                }catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        thread.start();
+    }
+}
--- app/src/main/java/com/dev/projectta/utils/PrefManager.java
+++ app/src/main/java/com/dev/projectta/utils/PrefManager.java
@@ -1,4 +1,18 @@
 package com.dev.projectta.utils;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class PrefManager {
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    Context mContext;
+
+    private static final String PREF_NAME = "Pref_SetUp";
+
+    public PrefManager(Context context){
+        this.mContext = context;
+        preferences = mContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        editor = preferences.edit();
+    }
 }
