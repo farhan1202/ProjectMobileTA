721ffb6e06de77ff2688efb5dd921a7086941948
farhan1202 <hafifi1202@gmail.com>
2020-07-18 18:14:44 +0700

    Connect to internet OK

--- app/src/main/java/com/dev/projectta/intro/RegisterActivity.java
+++ app/src/main/java/com/dev/projectta/intro/RegisterActivity.java
@@ -2,16 +2,29 @@ package com.dev.projectta.intro;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dev.projectta.R;
+import com.dev.projectta.utils.apihelper.ApiInterface;
+import com.dev.projectta.utils.apihelper.UtilsApi;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -26,12 +39,66 @@ public class RegisterActivity extends AppCompatActivity {
     @BindView(R.id.btnRegister)
     Button btnRegister;
 
+    ApiInterface apiInterface;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         ButterKnife.bind(this);
+        apiInterface = UtilsApi.getApiService();
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(editNama.getText().toString())){
+                    editNama.setError("Field is Empty");
+                }else if (TextUtils.isEmpty(editNoBP.getText().toString())){
+                    editNoBP.setError("Field is Empty");
+                }else if (TextUtils.isEmpty(editPassword.getText().toString())){
+                    editPassword.setError("Field is Empty");
+                }else if (TextUtils.isEmpty(editRePassword.getText().toString())){
+                    editRePassword.setError("Field is Empty");
+                }else if(!editPassword.getText().toString().equals(editRePassword.getText().toString())){
+                    editPassword.setError("Password doesnt match");
+                    editRePassword.setError("Password doesnt match");
+                }else{
+                    fetchData();
+                }
+            }
+        });
     }
+
+    private void fetchData() {
+        apiInterface.registerUser(editNoBP.getText().toString(),
+                editPassword.getText().toString(),
+                editNama.getText().toString()).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()){
+                    try {
+                        JSONObject jsonObject = new JSONObject(response.body().string());
+                        if (jsonObject.getString("STATUS").equals("200")){
+                            Toast.makeText(RegisterActivity.this, jsonObject.getString("MESSAGE") +"", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }else {
+                    Toast.makeText(RegisterActivity.this, "Something when wrong", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });
+    }
+
+
 
     public void login(View view) {
         Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
--- app/src/main/java/com/dev/projectta/utils/apihelper/ApiInterface.java
+++ app/src/main/java/com/dev/projectta/utils/apihelper/ApiInterface.java
@@ -1,4 +1,16 @@
 package com.dev.projectta.utils.apihelper;
 
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
 public interface ApiInterface {
+    //user
+    @FormUrlEncoded
+    @POST("regUser.php")
+    Call<ResponseBody> registerUser(@Field("nobp") String nobp,
+                                    @Field("password") String password,
+                                    @Field("nama") String name);
 }
